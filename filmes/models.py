d4ea3606ce0c34a2d7cb912c21976f6485237d83
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-04-23 20:44:26 -0300

    base do arquivo de catalogo

---
+++ filmes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    ano = models.IntegerField()
+    genero = models.CharField(max_length=50)
+    capa = models.TextField(default='')
+    
+    def __str__(self):
+        return self.titulo
---
+++ filmes/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Filme
+
+def lista_filmes(request):
+    filmes = Filme.objects.all()
+    return render(request, 'filmes/lista.html', {'filmes': filmes})
+
