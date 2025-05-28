acf598bcc707bf687590c4ba4be9b42d168b319d
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-05-28 20:48:26 -0300

    Criação das rotas de excluir, editar e incluir

--- filmes/urls.py
+++ filmes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.lista_filmes, name='lista_filmes')
+    path('', views.lista_filmes, name='lista_filmes'),
+    path('new/', views.criar_filmes, name='criar_filmes'),
+    path('edit/<int:pk>/',views.atualizar_filmes, name='atualizar_filmes'),
+    path('delete/<int:pk>/', views.deletar_filme, name='deletar_filmes')
     
 ]
--- filmes/views.py
+++ filmes/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Filme
+from .forms import FilmeForm
+
 
 def lista_filmes(request):
     filmes = Filme.objects.all()
     return render(request, 'filmes/lista.html', {'filmes': filmes})
+
+def criar_filmes(resquest):
+    if resquest.method == 'POST':
+        form = FilmeForm(resquest.POST, resquest.FILES)
+        if form.is_valid():
+            form.save()
+            return render('lista_filmes')
+    else:
+        form = FilmeForm()
+        return render(resquest, 'filmes/form_filme.html',{'form': form})
+
+def atualizar_filmes(resquest, pk):
+    filme= get_object_or_404(Filme, pk=pk)
+    if resquest.method == 'POST':
+        form = FilmeForm(resquest.POST, resquest.FILES, instance=filme)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_filmes')
+    else:
+        form = FilmeForm(instance=filme)
+    return render(request,'filmes/form_filme.html', {'form': form})
+
+def deletar_filme(resquest, pk):
+    filme =get_object_or_404(Filme, pk=pk)
+    if resquest.method == 'POST':
+        filme.delete()
+        return redirect('lista_filme')
+    return render(resquest, 'filmes/confirmar_exclusao.html', {'filme': filme})
 
