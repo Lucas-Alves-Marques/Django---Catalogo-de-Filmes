8deeb454158e7d1cbd94c9550373c7fdbe12b609
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-06-04 20:01:31 -0300

    Adição do Incluir, Editar e Excluir

--- filmes/urls.py
+++ filmes/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     
     path('', views.lista_filmes, name='lista_filmes'),
     path('new/', views.criar_filmes, name='criar_filmes'),
-    path('edit/<int:pk>/',views.atualizar_filmes, name='atualizar_filmes'),
+    path('edit/<int:pk>/', views.atualizar_filmes, name='atualizar_filmes'),
     path('delete/<int:pk>/', views.deletar_filme, name='deletar_filmes')
     
 ]
--- filmes/views.py
+++ filmes/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Filme
 from .forms import FilmeForm
 
-
 def lista_filmes(request):
     filmes = Filme.objects.all()
     return render(request, 'filmes/lista.html', {'filmes': filmes})
 
-def criar_filmes(resquest):
-    if resquest.method == 'POST':
-        form = FilmeForm(resquest.POST, resquest.FILES)
+def criar_filmes(request):
+    if request.method == 'POST':
+        form = FilmeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return render('lista_filmes')
+            return redirect('lista_filmes')
     else:
         form = FilmeForm()
-        return render(resquest, 'filmes/form_filme.html',{'form': form})
+        
+    return render(request, 'filmes/form_filme.html', {'form': form})
 
-def atualizar_filmes(resquest, pk):
+def atualizar_filmes(request, pk):
     filme= get_object_or_404(Filme, pk=pk)
-    if resquest.method == 'POST':
-        form = FilmeForm(resquest.POST, resquest.FILES, instance=filme)
+    if request.method == 'POST':
+        form = FilmeForm(request.POST, request.FILES, instance=filme)
         if form.is_valid():
             form.save()
             return redirect('lista_filmes')
@@ -28,10 +28,10 @@ def atualizar_filmes(resquest, pk):
         form = FilmeForm(instance=filme)
     return render(request,'filmes/form_filme.html', {'form': form})
 
-def deletar_filme(resquest, pk):
+def deletar_filme(request, pk):
     filme =get_object_or_404(Filme, pk=pk)
-    if resquest.method == 'POST':
+    if request.method == 'POST':
         filme.delete()
-        return redirect('lista_filme')
-    return render(resquest, 'filmes/confirmar_exclusao.html', {'filme': filme})
+        return redirect('lista_filmes')
+    return render(request, 'filmes/confirmar_exclusao.html', {'filme': filme})
 
